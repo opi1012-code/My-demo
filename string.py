3c84301dbe2236e638bcd9aee0ce15faeec9390b
Rabeya Afrin <rabeyaafrin023@gmail.com>
2026-05-01 22:56:53 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+#str1="this is a string"
+#str2="Rabeya Afrin.\nI am creating it python"
+#print(str1+str2)
+#length of str
+#print(len(str1))
+#indexing
+#print(str1[3])
+#slicing:str[strating index : end index]
+#print(str1[4:])
+#print(str1[4:7])
+#print(str1[:4])
+#print(str1[-4:-1])#sesh theke count kora hobe
+#function
+#print(str1.endswith("ing"))
+#print(str1.capitalize())
+#print(str1.replace("i","a"))
+#print(str1.find("i"))
+#print(str1.count("i"))
+x=str(input("Enter your 1st name:"))
+print(len(x))
+print(x.count("a"))
